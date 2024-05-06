23452f62016b3fc502416e9298f8125b1fea6bb9
dezdoesit <dez.blair06@gmail.com>
2024-05-06 09:47:51 -0500

    Initial Commit

---
+++ MTBView/MTBViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  MTBViewApp.swift
+//  MTBView
+//
+//  Created by dmoney on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct MTBViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
